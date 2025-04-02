3343e96e68c14df371fdd7bfa8569d499f7b0ce0
WSL work <maike.santana2@gmail.com>
2025-04-02 10:08:51 -0300

    correcting formatting details in home

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -1,41 +1,38 @@
 import '../../css/home.css';
 
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <main>
+        <div class="tasks-category-container">
+            <div class="tasks-header-container">
+                <p class="header-label">To-do</p>
+            </div>
+            <div class="new-task-button-container">
+                <button class="new-task-button">+</button>
+            </div>
+        </div>
+        <div class="tasks-category-container">
+            <div class="tasks-header-container">
+                <p class="header-label">In progress</p>
+            </div>
+            <div class="new-task-button-container">
+                <button class="new-task-button">+</button>
+            </div>
+        </div>
+        <div class="tasks-category-container">
+            <div class="tasks-header-container">
+                <p class="header-label">Done</p>
+            </div>
+            <div class="new-task-button-container">
+                <button class="new-task-button">+</button>
+            </div>
+        </div>
+    </main>
+`;
+
+
 export function loadHome() {
-    const template = document.createElement('template');
-
-    template.innerHTML = `
-        <main>
-            <div class="tasks-category-container">
-                <div class="tasks-header-container">
-                    <p class="header-label">To-do</p>
-                </div>
-                <div class="new-task-button-container">
-                    <button class="new-task-button">+</button>
-                </div>
-            </div>
-            <div class="tasks-category-container">
-                <div class="tasks-header-container">
-                    <div class="header-label">
-                        <p>In progress</p>
-                    </div>
-                </div>
-                <div class="new-task-button-container">
-                    <button class="new-task-button">+</button>
-                </div>
-            </div>
-            <div class="tasks-category-container">
-                <div class="tasks-header-container">
-                    <div class="header-label">
-                        <p>Done</p>
-                    </div>
-                </div>
-                <div class="new-task-button-container">
-                    <button class="new-task-button">+</button>
-                </div>
-            </div>
-        </main>
-    `;
-
 
     const main = template.content.querySelector('main').cloneNode(true);
 
