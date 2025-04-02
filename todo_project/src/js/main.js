07be86306e279ca29a7c17e8fb6ba2546bc80d50
WSL work <maike.santana2@gmail.com>
2025-04-02 09:15:58 -0300

    removing incorret todo_project folder

--- todo_project/src/js/main.js
+++
@@ -1,9 +0,0 @@
-import { loadNav } from "./components/nav";
-import { loadHome } from "./components/home";
-
-document.addEventListener("DOMContentLoaded", () => { 
-    loadNav(); 
-    loadHome();
-});
-
-console.log(window.innerWidth, window.innerHeight);
